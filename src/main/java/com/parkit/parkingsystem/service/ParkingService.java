b50cfc6fd655a08f2f3fc037d82a3e66a2eef09e
cariocaphil <cariocaphil@gmail.com>
2022-05-21 16:55:10 +0200

    add returning customer welcome message

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -45,6 +45,11 @@ public class ParkingService {
                 ticket.setInTime(inTime);
                 ticket.setOutTime(null);
                 ticketDAO.saveTicket(ticket);
+                boolean isReturningCustomer = ticketDAO.recurringCustomer(vehicleRegNumber);
+                ticket.setRecurringCustomer(isReturningCustomer);
+                if (isReturningCustomer) {
+                    System.out.println("Welcome back! As a recurring user of our parking lot, you'll benefit from a 5% discount.");
+                }
                 System.out.println("Generated Ticket and saved in DB");
                 System.out.println("Please park your vehicle in spot number:"+parkingSpot.getId());
                 System.out.println("Recorded in-time for vehicle number:"+vehicleRegNumber+" is:"+inTime);
